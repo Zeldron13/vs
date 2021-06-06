aa5fac822e8a572d4dee438d78d6710e0349d8c5
evgeny <zeka2111@bk.ru>
2021-06-06 20:58:16 +0300

    начал писать скрипт

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-console.log('ba' + +"erw");
+"use strict";
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели");
+
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actor: {},
+    geners: [],
+    privat: false
+};
+
+
+
+
+
+const a = prompt("Один из последних фильмов?"),
+      b = prompt("Насколько вы его оцените?"),
+      c = prompt("Один из последних фильмов?"),
+      d = prompt("Насколько вы его оцените?");
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+
+
+
+console.log(personalMovieDb);
